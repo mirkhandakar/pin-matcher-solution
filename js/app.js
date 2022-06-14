29d23f601b81d742c8044ec9f373a9b8bde39b58
mirkhandakar <mirkhandakar@gmail.com>
2022-06-14 16:29:48 +0600

    first commit

--- js/app.js
+++ js/app.js
@@ -14,3 +14,14 @@ function generatePin() {
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
 }
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    if (isNaN(number)) {
+        console.log()
+    }
+    const calcInput = document.getElementById('typed-numbers');
+    const previousNumber = calcInput.value;
+    const newNumber = previousNumber + number;
+    calcInput.value = newNumber;
+})
